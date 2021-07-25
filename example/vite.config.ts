f83ebebdf7577ff21c1de0dbb8fb4d773747469c
SepVeneto <sepveneto@qq.com>
2021-07-25 15:59:46 +0800

    外部引入

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
+export default defineConfig(({ command }) => {
+  const alias: Record<string, string> = command === 'serve' ? {} : {
+    'vue': 'https://unpkg.com/vue@3.1.5/dist/vue.runtime.esm-browser.prod.js'
+  };
+  return {
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias,
+    }
+  }
 })
