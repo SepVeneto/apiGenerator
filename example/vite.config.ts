f5e8b2f81048ab9eb364827c5599b2310668d2c6
SepVeneto <sepveneto@qq.com>
2021-07-25 16:27:13 +0800

    展示demo

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 export default defineConfig(({ command }) => {
-  const alias: Record<string, string> = command === 'serve' ? {} : {
-    'vue': 'https://unpkg.com/vue@3.1.5/dist/vue.runtime.esm-browser.prod.js'
+  const config = command === 'serve' ? {} : {
+    resolve: {
+      alias: {
+        'vue': 'https://unpkg.com/vue@3.1.5/dist/vue.runtime.esm-browser.prod.js'
+      }
+    }
   };
+  console.log(__dirname, path.resolve('..', __dirname, 'gh-pages'))
   return {
     plugins: [vue(), vueJsx()],
-    resolve: {
-      alias,
-    }
+    build: {
+      outDir: path.resolve('..', __dirname, 'gh-pages'),
+    },
+    ...config,
   }
 })
